377ade9ef4aaf4cb62af4aa6fb0fec2956855b6e
Indianrenters <Indianrenters@DESKTOP-08P2NI5>
2020-09-26 12:12:03 +0530

    All the pojo classes with JPA, Web, and MySQl dependencies.

---
+++ TradeSurveillanceProject/src/main/java/com/pojo/Customer.java
@@ -0,0 +1,26 @@
+package com.pojo;
+
+public class Customer {
+		int customerId;
+		String customerName;
+		long customerContact;
+		public int getCustomerId() {
+			return customerId;
+		}
+		public void setCustomerId(int customerId) {
+			this.customerId = customerId;
+		}
+		public String getCustomerName() {
+			return customerName;
+		}
+		public void setCustomerName(String customerName) {
+			this.customerName = customerName;
+		}
+		public long getCustomerContact() {
+			return customerContact;
+		}
+		public void setCustomerContact(long customerContact) {
+			this.customerContact = customerContact;
+		}
+		
+}
