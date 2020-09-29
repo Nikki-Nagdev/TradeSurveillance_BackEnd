6fb69cf97c23d064186c4b50208f42b26f50c230
indianrenters <indianrenters@DESKTOP-BRFD21N>
2020-09-29 11:14:16 +0530

    Database classes

--- TradeSurveillanceProject/src/main/java/com/pojo/MarketList.java
+++ TradeSurveillanceProject/src/main/java/com/pojo/MarketList.java
@@ -1,34 +1,86 @@
 package com.pojo;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity(name = "marketlist")
 public class MarketList {
-	String securityType;
-	String securityName;
-	int securityId;
-	double Price;
-	public String getSecurityType() {
-		return securityType;
+
+	@Id
+	private int securityId;
+	
+
+
+	@Column
+
+	private int security;
+	private String securityName;
+
+	private Date marketTimestamp;
+
+	private double currentMarketPrice;
+
+	public MarketList() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
-	public void setSecurityType(String securityType) {
-		this.securityType = securityType;
-	}
-	public String getSecurityName() {
-		return securityName;
-	}
-	public void setSecurityName(String securityName) {
-		this.securityName = securityName;
-	}
+
 	public int getSecurityId() {
 		return securityId;
 	}
+
 	public void setSecurityId(int securityId) {
 		this.securityId = securityId;
 	}
-	public double getPrice() {
-		return Price;
+
+	public int getSecurity() {
+		return security;
 	}
-	public void setPrice(double price) {
-		Price = price;
+
+	public void setSecurity(int security) {
+		this.security = security;
 	}
+
+	public String getSecurityName() {
+		return securityName;
+	}
+
+	public void setSecurityName(String securityName) {
+		this.securityName = securityName;
+	}
+
+	public Date getMarketTimestamp() {
+		return marketTimestamp;
+	}
+
+	public void setMarketTimestamp(Date marketTimestamp) {
+		this.marketTimestamp = marketTimestamp;
+	}
+
+	public double getCurrentMarketPrice() {
+		return currentMarketPrice;
+	}
+
+	public void setCurrentMarketPrice(double currentMarketPrice) {
+		this.currentMarketPrice = currentMarketPrice;
+	}
+
+	public MarketList(int securityId, int security, String securityName, Date marketTimestamp,
+			double currentMarketPrice) {
+		super();
+		this.securityId = securityId;
+		this.security = security;
+		this.securityName = securityName;
+		this.marketTimestamp = marketTimestamp;
+		this.currentMarketPrice = currentMarketPrice;
+	}
+
 	
+
 	
+
 }
--- TradeSurveillanceProject/src/main/java/com/pojo/Trade.java
+++ TradeSurveillanceProject/src/main/java/com/pojo/Trade.java
@@ -2,45 +2,145 @@ package com.pojo;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+
+@Entity(name="trade")
 public class Trade {
-	int tradeId; // for the trade table
-	Date tradeExecutionTime;				// date to be filled
-	int orderId;     // Order object to be created at the time of implementation
-	String brokerName;
+	
+
+	@Id
+	private int tradeId; // for the trade table
+	private Date tradeExecutionTime; // date to be filled
+	
+	private String brokerName;
+	
+	
+	private int securityId;
+
+	private int security;
+	private String securityName;
+	private boolean tradeType;
+	
+	private int marketPrice;
+	private int price;
+	
+	private boolean isChecked;
+	
+	private int quantity;
+	
+	
+	
+	
+	@ManyToOne
+	private Customer customer;
+	//int customerId=customer.getCustomerId();
+
+	public Trade() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	public int getTradeId() {
 		return tradeId;
 	}
+
 	public void setTradeId(int tradeId) {
 		this.tradeId = tradeId;
 	}
+
 	public Date getTradeExecutionTime() {
 		return tradeExecutionTime;
 	}
+
 	public void setTradeExecutionTime(Date tradeExecutionTime) {
 		this.tradeExecutionTime = tradeExecutionTime;
 	}
-	public int getOrderId() {
-		return orderId;
-	}
-	public void setOrderId(int orderId) {
-		this.orderId = orderId;
-	}
+
 	public String getBrokerName() {
 		return brokerName;
 	}
+
 	public void setBrokerName(String brokerName) {
 		this.brokerName = brokerName;
+	}
+
+	public int getSecurityId() {
+		return securityId;
+	}
+
+	public void setSecurityId(int securityId) {
+		this.securityId = securityId;
+	}
+
+	public int getSecurity() {
+		return security;
+	}
+
+	public void setSecurity(int security) {
+		this.security = security;
+	}
+
+	public String getSecurityName() {
+		return securityName;
+	}
+
+	public void setSecurityName(String securityName) {
+		this.securityName = securityName;
+	}
+
+	public boolean isTradeType() {
+		return tradeType;
+	}
+
+	public void setTradeType(boolean tradeType) {
+		this.tradeType = tradeType;
+	}
+
+	public int getMarketPrice() {
+		return marketPrice;
+	}
+
+	public void setMarketPrice(int marketPrice) {
+		this.marketPrice = marketPrice;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public boolean isChecked() {
+		return isChecked;
+	}
+
+	public void setChecked(boolean isChecked) {
+		this.isChecked = isChecked;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
 	}
 	
 	
 	
-//int customerId;
 	
-//	String tradeType; // alternate enum Type{}
-//	String securityType; 
-//	String securityName;
-//	int quantity;
-//	String brokerName;  // broker name
-//	int clientId; // for the customer table
-//	
+
 }
