3dcb61fc5d83791cf9d1e10e5088abbd48430d97
gaikwadsn99 <71120422+gaikwadsn99@users.noreply.github.com>
2020-09-29 16:46:52 +0530

    Trade repository package added

---
+++ TradeSurveillanceProject/src/main/java/com/repository/TradeRepository.java
@@ -0,0 +1,11 @@
+package com.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pojo.Trade;
+
+public interface TradeRepository extends JpaRepository<Trade, Integer>{
+	 
+
+
+}
