f18fd75756da53896b7cb4b813a00f458ec0424a
gaikwadsn99 <71120422+gaikwadsn99@users.noreply.github.com>
2020-09-30 15:55:30 +0530

    added URI for detection part

--- TradeSurveillanceProject/src/main/java/com/repository/TradeRepository.java
+++ TradeSurveillanceProject/src/main/java/com/repository/TradeRepository.java
@@ -1,11 +1,16 @@
 package com.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pojo.Trade;
 
+@Repository
 public interface TradeRepository extends JpaRepository<Trade, Integer>{
 	 
-
+ public List<Trade> findByIsChecked(Boolean a);
 
 }
